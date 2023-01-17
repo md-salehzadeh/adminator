947de2f61679f5a91cfa18293221805c95f2d98f
md.salehzadeh <md.salehzadeh@gmail.com>
2023-01-17 13:03:45 +0330

    compare

--- cmd/compare.go
+++ cmd/compare.go
@@ -11,20 +11,54 @@ var compareCmd = &cobra.Command{
 	Short: "Compare packages on the system versus packages on the file",
 	Long:  `Compare packages on the system versus packages on the file`,
 	Run: func(cmd *cobra.Command, args []string) {
-		runCompare()
+		runCompare(cmd)
 	},
 }
 
 func init() {
 	pkgsCmd.AddCommand(compareCmd)
+
+	compareCmd.Flags().BoolP("apply", "a", false, "whether to apply the changes or not")
 }
 
-func runCompare() {
+func runCompare(cmd *cobra.Command) {
 	installPkgs, uninstallPkgs := compare()
 
-	fmt.Println("Install Packages", installPkgs)
+	fmt.Println("----------------------")
 
-	fmt.Println("Uninstall Packages", uninstallPkgs)
+	if len(installPkgs) == 0 {
+		fmt.Println("No packages need to be installed")
+	} else {
+		fmt.Println("Install Packages:")
+
+		for _, pkg := range installPkgs {
+			fmt.Printf("\n%s", pkg.Name)
+		}
+	}
+
+	fmt.Println("\n----------------------")
+
+	if len(uninstallPkgs) == 0 {
+		fmt.Println("No packages need to be uninstalled")
+	} else {
+		fmt.Println("Uninstall Packages:")
+
+		for _, pkg := range uninstallPkgs {
+			fmt.Printf("\n%s", pkg.Name)
+		}
+	}
+
+	fmt.Println("\n----------------------")
+
+	apply, err := cmd.Flags().GetBool("apply")
+
+	if err != nil {
+		panic(err)
+	}
+
+	if (apply) {
+		fmt.Println("\nnow applying changes")
+	}
 }
 
 func compare() (installPkgs []Package, uninstallPkgs []Package) {
